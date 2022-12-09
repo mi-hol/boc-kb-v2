fb5bdcad8a4613d872404d2e99826f44554e577b
mi-hol <22799428+mi-hol@users.noreply.github.com>
2022-12-09 21:40:41 +0100

    add copyright owners

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Benelli Owners Club - KnowledgeBase</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mi-hol/boc-kb-v2',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'License: CC BY-NC-ND 4.0 {new Date().getFullYear()} © Errol Kowald, Michael Holzer.',
   },
 }
 
