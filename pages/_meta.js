8d71f868ba4b03de732a9593a2828c926069765b
mi-hol <22799428+mi-hol@users.noreply.github.com>
2024-10-22 17:29:55 +0200

    follow doc

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,10 +1,10 @@
 export default {
-  "index": "Introduction"
-  ,"BOC-KnowledgeBase": "Knowledge Base"
-  ,"01_General": "General"
-  ,"03_Maintenance": "Maintenance"
-  ,"05_Engine": "Engine"
-  ,"09_Electrical": "Electrical"
-  ,"10_Diagnostics": "Diagnostics"
-  ,"99_HowToEditSite": "How to Edit this Website"
+  index: 'Introduction'
+  ,BOC_KnowledgeBase: 'Knowledge Base'
+  ,'01_General': 'General'
+  ,03_Maintenance: 'Maintenance'
+  ,05_Engine: 'Engine'
+  ,09_Electrical: 'Electrical'
+  ,10_Diagnostics: 'Diagnostics'
+  ,99_HowToEditSite: 'How to Edit this Website'
 }
